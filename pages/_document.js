fad375fa9b8bf7abcdc9cc6734177930ce303131
Chiazor-Daniel <chiazordaniel317@gmail.com>
2024-09-18 18:56:18 +0100

    b;lah

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <Main />
+        <>
+          <Main />
+        </>
         <NextScript />
       </body>
     </Html>
